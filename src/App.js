d7c238f0b08d758618481d98f7e4dff6fe011944
Wesed <dev.wfeduardo@gmail.com>
2022-05-02 22:30:27 -0300

    Iniciando as rotas

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { GlobalStyle } from "./Components/Styles/Global";
 import Dark from "./Components/Styles/Dark";
 import { ThemeProvider } from "styled-components";
 import ConfigMenu from './Components/ConfigMenu';
 import Navbar from './Components/Navbar';
 import Header from './Components/Header/Header';
-import User from './Components/User/UserProfile';
-import UserExperience from "./Components/User/UserExperience";
-import Projetos from './Components/User/Projetos/Projetos';
-import Clientes from './Components/User/Clientes/Clientes';
-import Contato from './Components/User/Contato/Contato';
+import User from './Components/User/User';
 import Footer from './Components/Footer/Footer';
+import Detalhes from './Components/Detalhes/Detalhes';
 
 function App() {
   const [theme, setTheme] = React.useState(Dark);
   const [color, setColor] = React.useState('#FE3525');
 
   return (
-    <div>
+    <BrowserRouter>
       {/* O theme provider atua como o useContext, ele serve pra passar o theme pra todos os outros elementos filho */}
       <ThemeProvider theme={theme}>
           <GlobalStyle />
           <Header color={color}/>
-          <ConfigMenu theme={theme}  setTheme={setTheme} color={color} setColor={setColor}/>
-          <User color={color} />
-          <Footer />
           <Navbar color={color} theme={theme}/>
+          <ConfigMenu theme={theme}  setTheme={setTheme} color={color} setColor={setColor}/>
+
+          <Routes>
+            <Route path="/" element={<User color={color}/>} />
+            <Route path="projeto/:titulo" element={<Detalhes/>} />
+          </Routes>
+
+          <Footer />
       </ThemeProvider>
-    </div>
+    </BrowserRouter>
   );
 }
 
